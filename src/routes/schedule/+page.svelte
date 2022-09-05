845003a8f73548adbbbdb9b236f2b19ac96c5fc4
Tony Grimes <acidtone@tonygrimes.com>
2022-09-05 14:08:20 -0600

    migrated Schedule to component

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import { siteTitle } from '$lib/config'
-  import { schedule } from '$lib/schedule'
+  import Schedule from '$lib/components/Schedule.svelte'
   
 </script>
 <svelte:head>
@@ -8,22 +8,5 @@
 </svelte:head>
 <h1>Program Schedule</h1>
 <div class="container">
-	{#each schedule as week, index}
-	<section class="week">
-		<h2>Week {index + 1}</h2>
-		{#each week as day}
-		<article>
-			{#if day.type == 'lesson'}
-			<a href="courses/{day.code}/day-{day.day}">
-				<h3 class="{day.code}">{day.code} - Day {day.day}</h3>
-				<time>{day.date}</time>
-			</a>
-			{:else}
-			<h3 class="holiday">{day.label}</h3>
-			<time>{day.date}</time>
-			{/if}
-		</article>
-		{/each}
-	</section>
-	{/each}
+	<Schedule />
 </div>
