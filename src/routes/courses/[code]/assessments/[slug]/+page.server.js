3c7983b894394a3f410db1e64e55d1d0185b32f2
Tony Grimes <acidtone@tonygrimes.com>
2022-09-05 16:37:44 -0600

    cleanup

--- src/routes/courses/[code]/assessments/[slug]/+page.server.js
+++ src/routes/courses/[code]/assessments/[slug]/+page.server.js
@@ -1,16 +1,11 @@
 import { error } from '@sveltejs/kit'
 import { assessments } from '$lib/helpers/importAssessments';
-import { dueDates } from '$lib/due-dates.js'
 
 export const load = async ({ params }) => {
 	try {	
-		// Course code
-		const code = params.code;
-		const slug = params.slug;
-		
 		const assessment = assessments.find((item) => item.slug === params.slug && item.code === params.code);
-		
 		const content = await import(`../../../../../lib/content/assessments/${assessment.code}/${assessment.slug}.md`);
+
 		return {
 			content: content.default.render().html,
 			meta: {...assessment}
