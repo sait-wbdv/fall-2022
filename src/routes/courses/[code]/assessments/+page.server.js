1778f057619cf0de8d6187acb74ddea269239ece
Tony Grimes <acidtone@tonygrimes.com>
2022-09-05 18:23:16 -0600

    clean up

--- src/routes/courses/[code]/assessments/+page.server.js
+++ src/routes/courses/[code]/assessments/+page.server.js
@@ -1,18 +1,11 @@
 import { error } from '@sveltejs/kit'
 import { assessments } from '$lib/helpers/importAssessments.js';
-import { dueDates } from '$lib/due-dates.js'
 
 export const load = async ({ params }) => {
 	try {	
-		const assSchedule = dueDates.find((item) => item.code === params.code)?.dueDates || [];
-		const assList = assSchedule.map((assessment) => {
-			const {title, type, points, code} = assessments.find((item) => 
-			item.code === params.code && 
-			item.slug === assessment.slug)
-			return {...assessment, title, type, points, code }
-		}) || null;
+		const courseAssessments = assessments.filter((assessment) => assessment.code === params.code);
 		return {
-			assessments: assList,
+			assessments: courseAssessments,
 			code: params.code
 		}
 	} catch(err) {
